9ee9b0545dac93cd7099b9b1957cfb85ebf0f126
骑兵程序员 <hmjlr123@gmail.com>
2014-06-24 17:00:49 +0800

    readme

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 ﻿
 chrome.extension.sendRequest({ url: location.href }, function (response) {
   response && response.forEach(function (oaakey) {
-    $(document.body).append('<script type="text/javascript" src="' + oaakey.script + '?clear='+ new Date()*1 +'"></script>');
+    $('head').append('<script type="text/javascript" src="' + oaakey.script + '?clear='+ new Date()*1 +'"></script>');
   });
 });
